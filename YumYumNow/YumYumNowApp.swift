b576489244c22eae38d58cb9d4f19b799d04b372
mieng18 <mieng18@hotmail.com>
2023-07-09 17:23:27 -0700

    Initial commit

---
+++ YumYumNow/YumYumNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumYumNowApp.swift
+//  YumYumNow
+//
+//  Created by mai nguyen on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct YumYumNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
